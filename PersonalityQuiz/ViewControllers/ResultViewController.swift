15cef13558bea24c340579c70daaebab43fb7a59
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-02 12:44:43 +0300

    Code optimisation

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -22,29 +22,16 @@ class ResultViewController: UIViewController {
     }
 
     private func getResult() -> Animal? {
-        answers
-            .reduce(into: [:]) { counts, number in
+        answers.reduce(into: [:]) { counts, number in
                 counts[number.animal, default: 0] += 1 }
-            .sorted { $0.1 > $1.1 }
-            .first?.key
+               .sorted { $0.1 > $1.1 }
+               .first?.key
     }
         
     private func showResult() {
         guard let animal = getResult() else { return }
         
-        switch animal {
-        case .dog:
-            titleLabel.text = "Вы - \(Animal.dog.rawValue)"
-            descriptionLabel.text = Animal.dog.definition
-        case .cat:
-            titleLabel.text = "Вы - \(Animal.cat.rawValue)"
-            descriptionLabel.text = Animal.cat.definition
-        case .rabbit:
-            titleLabel.text = "Вы - \(Animal.rabbit.rawValue)"
-            descriptionLabel.text = Animal.rabbit.definition
-        case .turtle:
-            titleLabel.text = "Вы - \(Animal.turtle.rawValue)"
-            descriptionLabel.text = Animal.turtle.definition
-        }
+        titleLabel.text = "Вы - \(animal.rawValue)"
+        descriptionLabel.text = animal.definition
     }
 }
